261cb7f21c7cd73f8e261b9b64fe80168944d393
COMLAB <mvillena23-0351@cca.edu.ph>
2025-02-17 19:41:23 +0800

    Update create.php

--- create.php
+++ create.php
@@ -1,6 +1,8 @@
 <?php 
 	echo "Hello World";
 	var_dump($_POST);
+	$lastname = $_POST['last_name'];
+	$firstname = $_POST['first_name'];
 ?>
 
 <br>
@@ -21,4 +23,16 @@ if ($conn->connect_error) {
 }
 echo "Connected successfully";
 
+$sql = "INSERT INTO students (id, last_name,first_name)
+VALUES ('', '$lastname', '$firstname')";
+
+if ($conn->query($sql) === TRUE){
+	echo "<br>";
+	echo "<br>";
+	echo "<br>";
+
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
 ?>

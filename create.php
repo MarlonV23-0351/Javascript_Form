34d9a17ccc6f143f30af66271a20045f7074a176
COMLAB <mvillena23-0351@cca.edu.ph>
2025-02-17 19:24:56 +0800

    Update create.php

--- create.php
+++ create.php
@@ -2,3 +2,23 @@
 	echo "Hello World";
 	var_dump($_POST);
 ?>
+
+<br>
+<br>
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "lorraine";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+?>
